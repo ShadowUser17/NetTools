b9e7c2d0d8d441fc1e246e05c2d5c48e2130619c
ShadowUser17 <linuxforce17@gmail.com>
2020-08-06 11:58:35 +0300

    Updated: .gitignore
    Added functions:
     get_ip_type(ip)
     _tcpscan(target)

--- tcpscan.py
+++ tcpscan.py
@@ -4,12 +4,13 @@ import argparse
 import os
 import pathlib
 import socket
-import multiprocessing
+import ipaddress
+import multiprocessing as mp
 #
 #
 def _resolve(host):
     try:
-        (main, doms, ips) = socket.gethostbyaddr(host)
+        (main, __, ips) = socket.gethostbyaddr(host)
         return (ips[0], main)
     #
     except socket.gaierror: pass
@@ -17,9 +18,34 @@ def _resolve(host):
 #
 #
 def resolve_targets(targets):
-    pool = multiprocessing.Pool(multiprocessing.cpu_count())
+    pool = mp.Pool(mp.cpu_count())
     targets = pool.map(_resolve, targets)
     return list(filter(None, targets))
+#
+#
+def get_ip_type(ip):
+    types = {4: socket.AF_INET, 6: socket.AF_INET6}
+    ip = ipaddress.ip_address(ip)
+    return types[ip.version]
+#
+#
+def _tcpscan(target):
+    status = 1
+    sock_type = get_ip_type(target[0])
+    client = socket.socket(sock_type, socket.SOCK_STREAM)
+    #
+    try:
+        client.settimeout(float(16))
+        client.connect(target)
+    #
+    except socket.timeout: status = 2
+    except socket.error: status = 0
+    else: client.close()
+    #
+    return (target[1], status)
+#
+#
+def scan_ports(host): pass
 #
 #
 def read_input_list(fname):
@@ -31,8 +57,11 @@ def read_input_list(fname):
     return list(filter(None, data))
 #
 #
+def get_port_range(ports): pass
+#
+#
 if __name__ == '__main__':
     targets = read_input_list('./targets.txt')
-    targets = resolve_targets(targets)
-    for item in targets: print(item[0], item[1])
+    #targets = resolve_targets(targets)
+    #for item in targets: print(item[0], item[1])
     #print(targets)
