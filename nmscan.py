555cc5a9ebeba334e1eebf4b72bfa37bb5e72ac4
ShadowUser17 <linuxforce17@gmail.com>
2021-01-29 23:25:02 +0200

    Updated nmscan.py (Replace methods).

--- nmscan.py
+++ nmscan.py
@@ -5,21 +5,24 @@ from traceback import print_exc
 
 
 class BaseScan:
-    def __init__(self, hosts):
-        'hosts = ip/cidr, fout = enable output to file.'
-        self.hosts = ip.ip_network(hosts)
+    def __init__(self):
         self.time_format = '%Y-%m-%d'
-
         self._case_prefix = 'scan'
         self._case_rcode = None
+        self._case_list = []
         self._case_now = None
+
+        self._host_list = []
+        self._host_now = None
+
         self._get_cases()
         self._sort_cases()
 
     def __call__(self):
         'Start testing...'
-        for host in self.hosts.hosts():
-            self._exec_cases(str(host))
+        for host in self._host_list:
+            self._host_now = host
+            self._exec_cases()
 
     def _get_cases(self):
         prefix = self._case_prefix
@@ -32,36 +35,56 @@ class BaseScan:
             key=lambda item: item.split('_')[:-1],
             reverse=True)
 
-    @property
-    def cases(self):
-        return self._case_list
+    def _host_validate(self, host):
+        try:
+            return str(ip.ip_address(host))
+
+        except ValueError:
+            pass
+
+    def _hosts_filter(self):
+        tmp = filter(self._host_validate, self._host_list)
+        tmp = filter(None, tmp)
+        self._host_list = list(tmp)
+
+    def hosts_from_list(self, hosts):
+        self._host_list = iter(hosts)
+        self._hosts_filter()
+
+    def hosts_from_file(self, fname):
+        with open(fname) as fd:
+            hosts = filter(None, fd)
+            hosts = map(str.rstrip, hosts)
+            self._host_list = hosts
+            self._hosts_filter()
 
     def _get_date(self):
         date = datetime.now()
         return date.strftime(self.time_format)
 
-    def _get_output_fd(self, host):
-        date = self._get_date()
-        case = self._case_now
-        fname = '{}_{}-{}.log'.format(host, date, case)
+    def _get_output_fd(self):
+        fname = '{}_{}-{}.log'.format(
+            self._host_now,
+            self._get_date(),
+            self._case_now)
+
         return open(fname, 'wb')
 
-    def exec_cmd(self, args, host):
-        with self._get_output_fd(host) as fd:
-            cmd = '{} {}'.format(args, host)
+    def exec_cmd(self, cmd):
+        with self._get_output_fd() as fd:
             ps = sp.Popen(cmd, shell=True, stdout=fd, stderr=fd)
             self._case_rcode = ps.wait()
             return self._case_rcode
 
-    def _exec_cases(self, host):
+    def _exec_cases(self):
         for case in self._case_list:
             self._case_now = case
             func = getattr(self, case)
 
             try:
-                func(host)
+                func(self._host_now)
                 rcode = self._case_rcode
-                print('Task: {}:{}:{}'.format(host, case, rcode))
+                print('Task: {}:{}:{}'.format(self._host_now, case, rcode))
 
             except Exception:
                 print_exc()
