8aa2a82c9d2c65ecaaf923b12375d51ade94e7a7
ShadowUser17 <linuxforce17@gmail.com>
2021-04-05 19:48:37 +0300

    Updated gendict.py

--- gendict.py
+++ gendict.py
@@ -9,6 +9,7 @@ try:
     parser = argparse.ArgumentParser()
     parser.add_argument('scan_dir')
     parser.add_argument('dict_file')
+    parser.add_argument('-f', dest='files', help='Include files.', action='store_true')
     args = parser.parse_args()
 
     with open(args.dict_file, 'w') as fd:
@@ -17,10 +18,15 @@ try:
         os.chdir(scan_dir)
 
         with contextlib.redirect_stdout(fd):
-            for (root, dirs, _) in os.walk(scan_root):
-                for item in dirs:
-                    tmp = os.path.join(root, item)
+            for (root, dirs, files) in os.walk(scan_root):
+                for dir_item in dirs:
+                    tmp = os.path.join(root, dir_item)
                     print(tmp.lstrip(scan_root))
+
+                    if args.files:
+                        for file_item in files:
+                            tmp = os.path.join(root, dir_item, file_item)
+                            print(tmp.lstrip(scan_root))
 
 except Exception:
     traceback.print_exc()
