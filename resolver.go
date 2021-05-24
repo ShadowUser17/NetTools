c37c706496b2bc165ce5dd67140491f5594da85c
ShadowUser17 <linuxforce17@gmail.com>
2021-05-24 22:31:23 +0300

    Multiple updates of resolver.go

--- resolver.go
+++ resolver.go
@@ -4,17 +4,52 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"net"
 	"os"
 )
 
 func main() {
 	var args ArgList = ArgList{}
-	args.parseArgs()
+	args.ParseArgs()
 
-	/*var resolver DomResolver = DomResolver{}
-	resolver.setValue(*args.input)*/
+	var resolver DomResolver = DomResolver{}
 
+	if *args.isFile {
+		var reader FileReader = FileReader{}
+		err := reader.Open(*args.input)
+
+		if err != nil {
+			panic(err)
+		}
+
+		for {
+			line, err := reader.ReadLine()
+
+			if err == io.EOF {
+				break
+			}
+
+			resolver.SetValue(line)
+			err = resolver.Resolve()
+
+			if err != nil {
+				continue
+			}
+
+			resolver.PrintResults()
+		}
+
+		reader.Close()
+
+	} else {
+		resolver.SetValue(*args.input)
+		err := resolver.Resolve()
+
+		if err == nil {
+			resolver.PrintResults()
+		}
+	}
 }
 
 /*
@@ -27,23 +62,22 @@ type ArgList struct {
 	isInit bool
 }
 
-func (self *ArgList) initArgs() {
+func (self *ArgList) InitArgs() {
 	self.input = flag.String("i", "", "Set domain/ip/file.")
-	self.output = flag.String("o", "", "Set output file.")
 	self.isFile = flag.Bool("f", false, "Source is file.")
 
 	self.isInit = true
 }
 
-func (self *ArgList) parseArgs() {
+func (self *ArgList) ParseArgs() {
 	if self.isInit == false {
-		self.initArgs()
+		self.InitArgs()
 	}
 
 	flag.Parse()
 }
 
-func (self *ArgList) showArgs() string {
+func (self *ArgList) ShowArgs() string {
 	return fmt.Sprintf(
 		"{%s} {%s} {%t}", *self.input, *self.output, *self.isFile,
 	)
@@ -56,27 +90,29 @@ type FileReader struct {
 	fname string
 	fptr  *os.File
 	frdr  *bufio.Reader
+	ferr  error
 }
 
-func (self *FileReader) Open(fname string) {
-	var err error
-
+func (self *FileReader) Open(fname string) error {
 	self.fname = fname
-	self.fptr, err = os.Open(self.fname)
-	if err != nil {
-		panic(err)
+	self.fptr, self.ferr = os.Open(self.fname)
+
+	if self.ferr != nil {
+		return self.ferr
 	}
 
 	self.frdr = bufio.NewReader(self.fptr)
+	return nil
 }
 
-func (self *FileReader) Close() {
-	self.fptr.Close()
+func (self *FileReader) Close() error {
+	self.ferr = self.fptr.Close()
+	return self.ferr
 }
 
-func (self *FileReader) ReadLine() ([]byte, error) {
+func (self *FileReader) ReadLine() (string, error) {
 	line, _, err := self.frdr.ReadLine()
-	return line, err
+	return string(line), err
 }
 
 /*
@@ -88,63 +124,42 @@ type DomResolver struct {
 	results []string
 }
 
-func (self *DomResolver) setValue(val string) {
+func (self *DomResolver) SetValue(val string) {
 	if ip := net.ParseIP(val); ip != nil {
 		self.domAddr = val
-		self.isAddr = true
+		self.isAddr = false
+
 	} else {
 		self.domAddr = val
-		self.isAddr = false
+		self.isAddr = true
 	}
 }
 
-func (self *DomResolver) printResults() {
+func (self *DomResolver) PrintResults() {
 	for it := 0; it < len(self.results); it++ {
 		fmt.Printf("%s\n", self.results[it])
 	}
 }
 
-func (self *DomResolver) Resolve() {
+func (self *DomResolver) Resolve() error {
 	if self.isAddr {
 		res, err := net.LookupHost(self.domAddr)
+
 		if err != nil {
-			panic(err)
+			return err
 		}
+
 		self.results = res
 
 	} else {
 		res, err := net.LookupAddr(self.domAddr)
+
 		if err != nil {
-			panic(err)
+			return err
 		}
+
 		self.results = res
 	}
-}
 
-/*
-Write file...
-*/
-type FileWriter struct {
-	fname string
-	fptr  *os.File
-}
-
-func (self *FileWriter) Open(fname string) {
-	var err error
-
-	self.fname = fname
-	self.fptr, err = os.Open(self.fname)
-	if err != nil {
-		panic(err)
-	}
-}
-
-func (self *FileWriter) Close() {
-	self.fptr.Close()
-}
-
-func (self *FileWriter) Write(items []string) {
-	for it := 0; it < len(items); it++ {
-		self.fptr.Write([]byte(items[it]))
-	}
+	return nil
 }
